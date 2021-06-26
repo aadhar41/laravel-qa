28a26268c19a15cc2b23cd9aebf5089fb58ecc30
aadhar gaur <aadhar41@gmail.com>
2021-06-26 10:27:35 +0530

    Build Question Form

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -29,7 +29,8 @@ class QuestionsController extends Controller
      */
     public function create()
     {
-        //
+        $question = new Question();
+        return view('questions.create', $question);
     }
 
     /**
