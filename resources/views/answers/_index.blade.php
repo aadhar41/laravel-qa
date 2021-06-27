878c21b54f1301da341848fcde7894a9c8a0f784
aadhar gaur <aadhar41@gmail.com>
2021-06-27 21:53:15 +0530

    Update best_answer_id on questions table when an answer removed using eloquent event

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -20,7 +20,7 @@
                         <a href="#" title="This answer is not useful" class="vote-down off">
                             <i class="fas fa-caret-down fa-3x"></i>
                         </a>
-                        <a href="#" title="Mrak this answer as best answer." class="vote-accepted mt-2">
+                        <a href="#" title="Mrak this answer as best answer." class="{{$answer->status}} mt-2">
                             <i class="fas fa-check fa-lg"></i>
                         </a>
                     </div>
