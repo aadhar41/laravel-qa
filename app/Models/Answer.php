9b702ec9a32ec5ee1805d8d74c6cd5bfcabc088d
aadhar gaur <aadhar41@gmail.com>
2021-08-06 20:08:46 +0530

    Implement DRY principle on Answer and Question model

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Question;
 use App\Models\User;
+use App\Traits\VotableTrait;
 
 class Answer extends Model
 {
     use HasFactory;
+    use VotableTrait;
 
     protected $fillable = ['body', 'user_id'];
 
@@ -59,20 +61,5 @@ class Answer extends Model
     public function isBest()
     {
         return $this->id === $this->question->best_answer_id;
-    }
-
-    public function votes()
-    {
-        return $this->morphToMany(User::class, 'votable');
-    }
-
-    public function upVotes()
-    {
-        return $this->votes()->wherePivot('vote', 1);
-    }
-
-    public function downVotes()
-    {
-        return $this->votes()->wherePivot('vote', -1);
     }
 }
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 use App\Models\Answer;
+use App\Traits\VotableTrait;
 
 class Question extends Model
 {
     use HasFactory;
+    use VotableTrait;
 
     protected $fillable = ['title', 'body'];
 
@@ -79,20 +81,5 @@ class Question extends Model
     public function getFavoritesCountAttribute()
     {
         return $this->favorites->count();
-    }
-
-    public function votes()
-    {
-        return $this->morphToMany(User::class, 'votable');
-    }
-
-    public function upVotes()
-    {
-        return $this->votes()->wherePivot('vote', 1);
-    }
-
-    public function downVotes()
-    {
-        return $this->votes()->wherePivot('vote', -1);
     }
 }
